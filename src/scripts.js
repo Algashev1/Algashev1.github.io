872c792b346de11a2fb3224651767744e07683c6
Algashev1 <algashev@live.com>
2022-11-27 12:47:26 +0400

    Added files

---
+++ src/scripts.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const Path = {
+  1: "./examples/task1/task1.html",
+  2: "./examples/task2/task2.html",
+  3: "./examples/task3/task3.html",
+  4: "./examples/task4/task4.html",
+  5: "./examples/task5/task5.html",
+  6: "./examples/task6/task6.html",
+  7: "./examples/task7/task7.html",
+};
+
+function selectTask(numberOfTask) {
+  document.getElementById("iframe").setAttribute("src", Path[numberOfTask]);
+}
+
+function initPage() {
+  document.getElementById("iframe").setAttribute("src", Path[1]);
+}
+
+initPage();
