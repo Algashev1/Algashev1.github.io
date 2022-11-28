5a51b30b4a0737db2cbf85f18b629a1e2915eb6e
Artem Tarasov <art.tarasov0@gmail.com>
2022-11-28 16:36:05 +0400

    refactored routing

--- scripts.js
+++ scripts.js
@@ -2,19 +2,14 @@
 
 const FAQ_PAGE = "./faq/faq.html";
 const TASKS_PAGE = "./tasks/tasks.html";
+const exampleRegExp = /#example-\d+/;
 
-const Path = {
-  1: "./examples/task1/task1.html",
-  2: "./examples/task2/task2.html",
-  3: "./examples/task3/task3.html",
-  4: "./examples/task4/task4.html",
-  5: "./examples/task5/task5.html",
-  6: "./examples/task6/task6.html",
-  7: "./examples/task7/task7.html",
-};
+function generateTaskPath(numberOfTask) {
+  return `/examples/task${numberOfTask}/task${numberOfTask}.html`;
+}
 
 function selectTask(numberOfTask) {
-  document.getElementById("iframe").setAttribute("src", Path[numberOfTask]);
+  document.getElementById("iframe").setAttribute("src", generateTaskPath(numberOfTask));
 }
 
 function openFAQ() {
@@ -22,47 +17,19 @@ function openFAQ() {
 }
 
 function initPage() {
-  document.getElementById("iframe").setAttribute("src", Path[1]);
+  document.getElementById("iframe").setAttribute("src", generateTaskPath(1));
 }
 
 function goToPage() {
   const hash = window.location.hash;
-  switch (hash) {
-    case "#example-1": {
-      document.getElementById("iframe").setAttribute("src", Path[1]);
-      break;
-    }
-    case "#example-2": {
-      document.getElementById("iframe").setAttribute("src", Path[2]);
-      break;
-    }
-    case "#example-3": {
-      document.getElementById("iframe").setAttribute("src", Path[3]);
-      break;
-    }
-    case "#example-4": {
-      document.getElementById("iframe").setAttribute("src", Path[4]);
-      break;
-    }
-    case "#example-5": {
-      document.getElementById("iframe").setAttribute("src", Path[5]);
-      break;
-    }
-    case "#example-6": {
-      document.getElementById("iframe").setAttribute("src", Path[6]);
-      break;
-    }
-    case "#example-7": {
-      document.getElementById("iframe").setAttribute("src", Path[7]);
-      break;
-    }
-    case "#faq": {
-      document.getElementById("iframe").setAttribute("src", FAQ_PAGE);
-      break;
-    }
-    case "#tasks": {
-      document.getElementById("iframe").setAttribute("src", TASKS_PAGE);
-    }
+  const iframe = document.getElementById("iframe")
+
+  if (exampleRegExp.exec(hash)) {
+    const [numberOfTask] = hash.match(/\d+/);
+    iframe.setAttribute("src", generateTaskPath(numberOfTask));
+  } else {
+    const pathWithoutHash = hash.slice(1);
+    iframe.setAttribute("src", `/${pathWithoutHash}/${pathWithoutHash}.html`);
   }
 }
 
