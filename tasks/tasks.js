38b1914b0e137ca8c58f83791358a09034ae63fa
Algashev1 <algashev@live.com>
2023-04-03 14:04:25 +0400

    Добавлено второе задание

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,5 +1,11 @@
 const container = document.getElementById("main-content");
 
+const videoLinks = [
+  "",
+  "https://bbb.ssau.ru:8443/playback/presentation/2.3/87b6ec23de33ee4841108040bdb92d69753a78f5-1679921954769",
+  "",
+];
+
 function buildTasksList() {
   let html = `
     <h3 id="task-0">
@@ -11,20 +17,20 @@ function buildTasksList() {
     </p>
   `;
 
-  for (let i = 1; i <= 1; i++) {
+  for (let i = 1; i <= 2; i++) {
     html += `
         <h3 id="task-${i}">
             <a href="#task-${i}"></a>
             Занятие ${i}
         </h3>
         <p class="mb-4">
-            <a target="_blank" href="../pdf/tasks/${i}.pdf">⚒️ Задание</a>
-            <a target="_blank" href="../pdf/presentations/${i}.pdf">📄 Материалы</a>
+            <a target="_blank" href="../pdf/tasks/${i}.pdf">⚒️ Задание </a>
+            <a target="_blank" href="../pdf/presentations/${i}.pdf">📄 Презентация </a>
+            <a target="_blank" href="${videoLinks[i]}">🎞 Запись лекции </a>
         </p>
         `;
   }
 
-  console.log(container);
   container.innerHTML = html;
 }
 
