b4e081707443c8706c9d7d87c4d916cc5c854a2d
Algashev1 <algashev@live.com>
2023-04-24 19:25:28 +0400

    Добавлены материалы для 5-го занятия

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -6,6 +6,7 @@ const videoLinks = [
   "https://bbb.ssau.ru:8443/playback/presentation/2.3/87b6ec23de33ee4841108040bdb92d69753a78f5-1680526530305",
   "https://bbb.ssau.ru:8443/playback/presentation/2.3/87b6ec23de33ee4841108040bdb92d69753a78f5-1681138516254",
   "https://bbb.ssau.ru:8443/playback/presentation/2.3/87b6ec23de33ee4841108040bdb92d69753a78f5-1681736160367",
+  "https://bbb.ssau.ru:8443/playback/presentation/2.3/87b6ec23de33ee4841108040bdb92d69753a78f5-1682341002764",
 ];
 
 function buildTasksList() {
@@ -19,7 +20,7 @@ function buildTasksList() {
     </p>
   `;
 
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= 5; i++) {
     html += `
         <h3 id="task-${i}">
             <a href="#task-${i}"></a>
